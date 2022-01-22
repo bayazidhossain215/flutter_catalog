7970b1e50590ea911cc2fcf44c5a0fbc85ce939c
bayazidhossain215 <bayazidhossain215@gmail.com>
2022-01-22 12:34:20 +0600

    Day 3 Completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
     
     return Scaffold(
       appBar: AppBar(
-        title: Text("catalog app"),
+        title: Text("Catalog app"),
       ),
       body: Center(
         child: Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/home_page.dart';
+import 'package:flutter_catalog/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+    
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark),
+        initialRoute: "/home",
+        routes: {
+          "/": (context)=> LoginPage(),
+          "/home": (context)=> HomePage(),
+          "/login": (context)=> LoginPage(),
+
+        
+
+        },
+      
     );
   }
 }
